608ebe047e15d67c4d9ba9a1b7a779dfa683f719
azimmalik9 <azim.malik9@gmail.com>
2018-08-23 09:55:44 +0100

    Updated to use POM initElements

--- src/automationWebsite/Homepage.java
+++ src/automationWebsite/Homepage.java
@@ -1,48 +1,48 @@
 package automationWebsite;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Homepage {
 	WebDriver driver;
-	By search = By.name("submit_search");
-	By searchTerm = By.name("search_query");
-	By productName = By.xpath("//*[@id=\"center_column\"]/ul/li/div/div[2]/h5/a");
+
+	@FindBy(name="submit_search")
+	private WebElement search;
 	
+	@FindBy(name="search_query")
+	private WebElement searchTerm;
+	
+	@FindBy(xpath = "//*[@id=\"center_column\"]/ul/li/div/div[2]/h5/a")
+	private WebElement productName;
+
 	public Homepage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 		
 	}
 	
 	public void setSearchTerm(String strSearchTerm) {
-
-		
-		driver.findElement(searchTerm).sendKeys(strSearchTerm);
+		searchTerm.sendKeys(strSearchTerm);
 
 	}
 	
 	//Click on search button
 
     public void beginSearch(){
-
-            
-			driver.findElement(search).click();
-
+			search.click();
     }
     
     public String getProductName(){
-
-        return    driver.findElement(productName).getText();
-
+        return    productName.getText();
        }
     
     public void search(String searchTerm) {
     	// Fill search field
-    	
     	this.setSearchTerm(searchTerm);
     	
     	// Click Search button
-
         this.beginSearch();    
     }
 }
--- src/test/TestHomeSearch.java
+++ src/test/TestHomeSearch.java
@@ -4,56 +4,59 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import automationWebsite.Homepage;
 
-	public class TestHomeSearch {
+public class TestHomeSearch {
 
-		WebDriver driver;
-		Homepage home;
+	WebDriver driver;
+	Homepage home;
 
-		@BeforeTest
+	@Before
 
-		public void setup() {
+	public void setup() {
 
-			driver = new ChromeDriver();
+		driver = new ChromeDriver();
 
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-			driver.get("http://automationpractice.com/index.php");
-
-		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		/**
-		 * 
-		 * This test case will enter a search query in http://automationpractice.com/index.php
-		 * 
-		 * Assert that a particular item was found
-		 * 
-		 */
+		driver.get("http://automationpractice.com/index.php");
 
-		@Test(priority = 0)
-		public void test_Home_Page_Appear_Correct() {
-
-			// Create User Page object
-
-			home = new Homepage(driver);
-
-			// search for a product
-
-			home.setSearchTerm("t-shirt");
-
-			home.beginSearch();
-
-
-			// Verify product is visible
-
-			Assert.assertTrue(home.getProductName().contains("Faded Short Sleeve T-shirts"));
-
-		}
 
 	}
 
+	/**
+	 * 
+	 * This test case will enter a search query in
+	 * http://automationpractice.com/index.php
+	 * 
+	 * Assert that a particular item was found
+	 * 
+	 */
+
+
+	@Test
+	public void test_Home_Page_Appear_Correct() {
+
+		// Create User Page object
+		home = new Homepage(driver);
+
+		// search for a product
+		home.setSearchTerm("t-shirt");
+
+		home.beginSearch();
+
+		// Verify product is visible
+		Assert.assertTrue(home.getProductName().contains("Faded Short Sleeve T-shirts"));
+
+	}
+
+	@After
+	public void close() {
+		driver.close();
+	}
+}
